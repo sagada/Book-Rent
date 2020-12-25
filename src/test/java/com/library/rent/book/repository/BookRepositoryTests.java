0b2e23d78b07be093d3e39f5b928f3cdd9ec1b26
ysh <wlatmdqja@naver.com>
2020-12-25 13:35:44 +0900

    feat Test 코드 수정, Junit5

--- src/test/java/com/library/rent/book/repository/BookRepositoryTests.java
+++ src/test/java/com/library/rent/book/repository/BookRepositoryTests.java
@@ -2,17 +2,12 @@ package com.library.rent.book.repository;
 
 import com.library.rent.web.book.domain.Book;
 import com.library.rent.web.book.repository.BookRepository;
-import com.library.rent.web.member.domain.Member;
 import com.library.rent.web.member.repository.MemberRepository;
-import org.junit.Before;
-import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
-
 import javax.transaction.Transactional;
-
-
+import org.junit.jupiter.api.Test;
 
 @Sql("classpath:sql/book-search-test.sql")
 @Transactional
@@ -20,24 +15,13 @@ import javax.transaction.Transactional;
 public class BookRepositoryTests {
 
     @Autowired
-    private BookRepository bookRepository;
+    BookRepository bookRepository;
 
     @Autowired
-    private MemberRepository memberRepository;
+    MemberRepository memberRepository;
 
-    @Before
-    public void 테스트_멤버_초기화()
-    {
-        Member testMember = Member.builder()
-                .name("ADMIN")
-                .id("ADMIN")
-                .build();
-
-        memberRepository.save(testMember);
-    }
 
     @Test
-    @Transactional
     public void 책_추가하기()
     {
         for (int i = 1; i <= 100; i++)
