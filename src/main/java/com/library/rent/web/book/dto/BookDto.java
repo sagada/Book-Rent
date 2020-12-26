ae84d9b9d358c7ccfa5c8b47c9a7ade3b70cc8a2
saga <wlatmdqja@naver.com>
2020-12-27 03:39:01 +0900

    feat query dsl 임시 막기 build.gradle 수정

--- src/main/java/com/library/rent/web/book/dto/BookDto.java
+++ src/main/java/com/library/rent/web/book/dto/BookDto.java
@@ -8,7 +8,6 @@ import javax.validation.Valid;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
